9d8d55c5dcf170aee2aa3877941fad1b365f021b
sfrj <some@email.com>
2016-07-12 06:55:43 +0100

    Logging support

--- src/main/java/application/services/GameBoardService.java
+++ src/main/java/application/services/GameBoardService.java
@@ -3,9 +3,15 @@ package application.services;
 import application.model.GameBoard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.System.lineSeparator;
 
 @Component
 public class GameBoardService {
+
+    private  Logger logger = LoggerFactory.getLogger(GameBoardService.class);
 
     private SelectionValidator selectionValidator;
     private ScoreResolver scoreResolver;
@@ -38,9 +44,17 @@ public class GameBoardService {
         symbolResolver.resolvePosition(currentGame);
 
         if(scoreResolver.winnerFound(currentGame) || boardCleanup.isFull(currentGame)) {
+            logEndOfGame(currentGame);
             boardCleanup.empty(currentGame);
         }
 
         return currentGame;
+    }
+
+    private void logEndOfGame(GameBoard currentGame) {
+        logger.info(currentGame.getGameId() + " ended " + lineSeparator() + "board:" + lineSeparator() +
+                currentGame.getBottomleft() + currentGame.getTop() + currentGame.getTopright() + lineSeparator() +
+                currentGame.getLeft() + currentGame.getMiddle() + currentGame.getRight() + lineSeparator() +
+                currentGame.getBottomleft() + currentGame.getBottom() + currentGame.getBottomright());
     }
 }
